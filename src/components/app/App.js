39c4b7c12968313af6c055670b08433b642f485b
Joshua <mwalejoshua89@gmail.com>
2022-07-28 09:22:39 +0300

    I re-wrote my index.css file adding desired Navbar styling

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,16 @@
-import "./App.css";
+import React from "react";
 import Navbar from "../navbar/Navbar";
 import MovieList from "../movielist/MovieList";
 import Movie from "../movie/Movie";
 import MovieForm from "../movieform/MovieForm";
 
+
 function App() {
+    
     return(
-        <div>
-            <Navbar/>
-            <MovieList/>
-            <Movie/>
-            <MovieForm/>
+        <div className="App">
+            <Navbar />
+            
         </div>
     );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,22 @@
 import React from "react";
-import "./Navbar.css"
 
 function Navbar() {
-    return(
-        <div className="topNavbar">
-            <a className="active" href="#movielist">MovieList</a>
-            <a href="#movie">Movie</a>
-            <a href="#movie-form">MovieForm</a>
-        </div>
+    const links = ["MovieList", "Movie", "MovieForm"];
+
+    const navlinks = links.map((link) => (
+        <a key={link} href={"#" + link.toLowerCase()}>
+            {link}
+        </a>
+    ));
+    return (
+    <div className="topNavbar">
+        <nav>
+            {navlinks}
+        </nav>
+    </div>
     )
+
+
 }
 
 export default Navbar;
