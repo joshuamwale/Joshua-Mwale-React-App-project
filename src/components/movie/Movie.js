9053042cdcf7e889b84d01abbce08771c3287948
Joshua <mwalejoshua89@gmail.com>
2022-07-29 04:16:51 +0300

    Added a Yet to watch and delete buttons

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Movie({item}) {
+    const [displayText, setDisplayText] = useState('Click');
+
+    //adding a button to Movie component
+
+    function handleClick() {
+        setDisplayText('Yet to watch');
+    }
+
+    function handleDeleteClick() {
+        fetch(`http://localhost:8000/movies/${item.id}`, {
+            method: "DELETE",
+        })
+        .then((response) => response.json())
+        .then(() => console.log("deleted"));
+    }
  return(
     <div>
         <h3>{item.title}</h3>
         <img src={item.image} alt="movie link"></img>
-        <p>{item.description}</p>
-
+        <p>
+            <strong>{item.description}</strong>
+        </p>
+        <button onClick={handleClick}>{displayText}</button> <br></br>
+        <button className="remove" onClick={handleDeleteClick}>
+            Delete
+        </button>
     </div>
- )
+ );
 }
 export default Movie;
