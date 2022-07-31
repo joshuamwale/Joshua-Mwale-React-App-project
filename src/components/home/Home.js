e91757e4b4ae38e8139cd4d132b3fffa3a950c29
Joshua <mwalejoshua89@gmail.com>
2022-07-31 14:13:30 +0300

    Modified my home component and dbjson file

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,9 +5,9 @@ function Home() {
         <div id="home">
             <center style={{marginTop: 5 +'em', fontSize: 20 + 'px'}}>
                 <p>
-                This is a premier site to search your favorite movies.<br></br>
-                The plan is simple: search and watch at your convenience.
-                Search for movies. Add your favorites too.
+                This is a premier site that lists your favorite movies drawn across decades.<br></br>
+                The plan is simple: scroll through the list to find your favorite movie.
+                Add your favorites too with a description.
                 </p>
             </center>
         </div>
